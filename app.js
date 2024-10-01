3206d1d13feb8f6b311d6ccd6b19cb3de76f4b73
zteplez <asibrahim98@gmail.com>
2024-10-01 23:40:59 +0300

    [feat]: Implement calculation function

--- app.js
+++ app.js
@@ -3,30 +3,70 @@ let buttons = document.querySelectorAll(".btn");
 let firstNum;
 let secondNum;
 let operator = null;
+let isOperatorPressed = false;
 
 buttons.forEach((btn) => {
-    btn.addEventListener("click", () => { 
+    btn.addEventListener("click", () => {
+        const value = btn.textContent;
+
         if (screen.innerText == "0") screen.textContent = "";
 
-        if(isNaN(btn.textContent)){
-            console.log(btn.textContent)
-            if(operator == null){
-                operator = btn.textContent;
-            }else{
-                alert( "You cant do that!");
-                return
-            }
-        }else{
+        if (isNaN(value)) {
+            switch (value) {
+                case "DELETE":
+                    deleteChar();
+                    break;
+                case "CLEAR":
+                    clear();
+                    break;
+                case "=":
+                    isOperatorPressed = false;
+                    parseCalculation();
+                    break;
+                default:
+                    if (!isOperatorPressed) {
+                        operator = value;
+                        screen.innerText += value;
+                        isOperatorPressed = true;
+                    } else {
+                        parseCalculation();
+                        screen.innerText += value;
+                    }
 
+                    break;
+            }
+
+        } else {
+            screen.innerText += btn.innerText;
         }
 
-        screen.innerText += btn.textContent;
-        displayValue = screen.textContent;
     });
 });
 
-
+function parseCalculation() {
+    let str = screen.textContent;
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i).match(/[×/+÷-]/)) {
+            operator = str.charAt(i);
+            firstNum = str.substr(0, i);
+            secondNum = str.substr(i + 1);
+            
+            if(firstNum && secondNum){
+                screen.innerText = operate(Number(firstNum), Number(secondNum), operator);
+            }else{
+                screen.innerText = "0";
+            }
+            break;
+        }
+    }
+}
 function operate(x, y, operator) {
+    if (firstNum == "" || secondNum == "" || operator == null) {
+        alert("Invalid input!");
+        return 0;
+    }
+
+    isOperatorPressed = false;
     switch (operator) {
         case "+":
             return add(x, y);
@@ -34,13 +74,14 @@ function operate(x, y, operator) {
         case "-":
             return sub(x, y);
             break;
-        case "*":
+        case "×":
             return multiply(x, y);
             break;
-        case "%":
-            return divide(x, y);
+        case "÷":
+            return y === 0 ? "Error" : divide(x, y);
             break;
         default:
+            return "0";
             break;
     }
 }
@@ -49,11 +90,15 @@ function sub(x, y) { return x - y; }
 function multiply(x, y) { return x * y; }
 function divide(x, y) { return x / y; }
 function clear() {
-    console.log("clear func called")
     screen.innerText = "0";
-    firstNum = 0;
-    secondNum = 0;
+    firstNum = null;
+    secondNum = null;
     operator = null;
+    isOperatorPressed = false;
+}
+function deleteChar() {
+    screen.innerText = screen.innerText.slice(0, -1);
+    if (screen.innerText === "") screen.innerText = "0";
 }
 
 document.querySelector("#clear").addEventListener("click", clear);
